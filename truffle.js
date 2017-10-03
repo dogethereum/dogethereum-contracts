1c8ec0b48a88d690740d23dbefca39d61b79fe9d
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2017-10-03 17:13:04 -0300

    raise gas limit, decrease gas price

--- truffle.js
+++ truffle.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gas: 4712388000,
+      gasPrice: 200000000
     }
   }
 };
