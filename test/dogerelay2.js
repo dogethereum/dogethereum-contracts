289a71ca5362eb93b39634e0d3ce4e469bda5d57
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2017-10-06 14:30:03 -0300

    dogerelay2.js working

--- test/dogerelay2.js
+++ test/dogerelay2.js
@@ -2,26 +2,20 @@ var DogeRelay = artifacts.require("./DogeRelay.sol");
 
 
 contract('DogeRelay', function(accounts) {
-  it("Set initial parent", function() {
+  it("setInitialParent, storeBlockHeader, getBlockchainHead, getBlockHeader, getPrevBlock, m_getTimestamp, m_getBits", function() {
     var dr;    
-    var block333000Hash;
-    var block333001Hash;
-    var block333001Header;
+    var block333000Hash = "0x000000000000000008360c20a2ceff91cc8c4f357932377f48659b37bb86c759";
+    var block333001Hash = "0x000000000000000010e318d0c61da0b84246481d9cc097fda9327fe90b1538c1";
+    // version = 2
+    // hashPrevBlock = 0x000000000000000008360c20a2ceff91cc8c4f357932377f48659b37bb86c759
+    // hashMerkleRoot = 0xf6f8bc90fd41f626705ac8de7efe7ac723ba02f6d00eab29c6fe36a757779ddd
+    // time = 1417792088
+    // bits = 0x181b7b74
+    // nonce = 796195988
+    // blockNumber = 333001
+    var block333001Header = "0x0200000059c786bb379b65487f373279354f8ccc91ffcea2200c36080000000000000000dd9d7757a736fec629ab0ed0f602ba23c77afe7edec85a7026f641fd90bcf8f658ca8154747b1b1894fc742f";
     return DogeRelay.deployed().then(function(instance) {      
       dr = instance;
-      block333000HashReturnValue = "000000000000000008360c20a2ceff91cc8c4f357932377f48659b37bb86c759";
-      block333000Hash = "0x" + block333000HashReturnValue;
-      block333001HashReturnValue = "000000000000000010e318d0c61da0b84246481d9cc097fda9327fe90b1538c1";
-      block333001Hash = "0x" + block333001HashReturnValue;
-
-      // version = 2
-      // hashPrevBlock = 0x000000000000000008360c20a2ceff91cc8c4f357932377f48659b37bb86c759
-      // hashMerkleRoot = 0xf6f8bc90fd41f626705ac8de7efe7ac723ba02f6d00eab29c6fe36a757779ddd
-      // time = 1417792088
-      // bits = 0x181b7b74
-      // nonce = 796195988
-      // blockNumber = 333001
-      block333001Header = "0x0200000059c786bb379b65487f373279354f8ccc91ffcea2200c36080000000000000000dd9d7757a736fec629ab0ed0f602ba23c77afe7edec85a7026f641fd90bcf8f658ca8154747b1b1894fc742f";
       return dr.setInitialParent(block333000Hash, 333000, 1, {from: accounts[0]}); 
     }).then(function(result) {
       //assert.equal(result, true, "result should be true");
@@ -30,13 +24,13 @@ contract('DogeRelay', function(accounts) {
       //assert res['output'] == 300000
       return dr.getBlockchainHead.call();
     }).then(function(result) {
-      assert.equal(formatHexUint32(result.toString(16)), block333001HashReturnValue, "chain head hash is not the expected one");
+      assert.equal(formatHexUint32(result.toString(16)), remove0x(block333001Hash), "chain head hash is not the expected one");
       return dr.getBlockHeader.call(block333001Hash);
     }).then(function(result) {
       assert.equal(result, block333001Header, "chain head header is not the expected one");
       return dr.getPrevBlock.call(block333001Hash);
     }).then(function(result) {
-      assert.equal(formatHexUint32(result.toString(16)), block333000HashReturnValue, "prev block hash is not the expected one");
+      assert.equal(formatHexUint32(result.toString(16)), remove0x(block333000Hash), "prev block hash is not the expected one");
       return dr.m_getTimestamp.call(block333001Hash);
     }).then(function(result) {
       assert.equal(result.toNumber(), 1417792088, "timestamp is not the expected one");
@@ -55,3 +49,9 @@ function formatHexUint32(str) {
     }  
     return str;
 }
+
+function remove0x(str) {
+    return str.substring(2);
+}
+
+
