f0ed6995d791f38d8b9c35c52e318daaf1c71775
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2017-10-12 21:03:44 -0300

    Bitcoin Processor

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,14 @@
 //var Constants = artifacts.require("./Constants.sol");
 //var DogeChain = artifacts.require("./DogeChain.sol");
 var DogeRelay = artifacts.require("./DogeRelay.sol");
+var BitcoinProcessor = artifacts.require("./BitcoinProcessor.sol");
 
 module.exports = function(deployer) {
   //deployer.deploy(Constants);
   //deployer.deploy(DogeChain);
   //deployer.link(Constants, DogeRelay);
   //deployer.link(DogeChain, DogeRelay);
-  deployer.deploy(DogeRelay);
+  deployer.deploy(DogeRelay).then(function(){
+	  deployer.deploy(BitcoinProcessor, DogeRelay.address);
+  });
 };
