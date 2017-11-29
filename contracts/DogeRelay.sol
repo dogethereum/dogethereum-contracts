fc126446d49e5d63610f420f5ddc1e9199f20500
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2017-11-29 13:51:38 -0300

    code clean up

--- contracts/DogeRelay.sol
+++ contracts/DogeRelay.sol
@@ -530,28 +530,6 @@ contract DogeRelay {
     return result;
   }
 
-	// return the difference between the chainWork at
-	// the blockchain Tip and its 10th ancestor
-	//
-	// this is not needed by the relay itself, but is provided in
-	// case some contract wants to use the chainWork or Bitcoin network
-	// difficulty (which can be derived) as a data feed for some purpose
-	function getAverageChainWork() returns (uint) {
-	    uint blockHash = bestBlockHash;
-
-	    uint128 chainWorkTip = m_getScore(blockHash);
-
-	    uint8 i = 0;
-	    while (i < 10) {
-	        blockHash = getPrevBlock(blockHash);
-	        i += 1;
-	    }
-
-	    uint128 chainWork10Ancestors = m_getScore(blockHash);
-
-	    return (chainWorkTip - chainWork10Ancestors);
-	}
-
 
 
 	// For a valid proof, returns the root of the Merkle tree.
--- contracts/DogeRelayForTests.sol
+++ contracts/DogeRelayForTests.sol
@@ -3,7 +3,7 @@ import "./DogeRelay.sol";
 
 contract DogeRelayForTests is DogeRelay {
   function bytesToBytes32Public(bytes b) public pure returns (bytes32) {
-  	return bytesToBytes32(b);
+    return bytesToBytes32(b);
   }
 
   function sliceArrayPublic(bytes original, uint32 offset, uint32 endIndex) public pure returns (bytes result) {
@@ -23,7 +23,31 @@ contract DogeRelayForTests is DogeRelay {
   // return the chainWork of the Tip
   // http://bitcoin.stackexchange.com/questions/26869/what-is-chainwork
   function getChainWork() public view returns (uint128) {
-	return m_getScore(bestBlockHash);
+  	return m_getScore(bestBlockHash);
   }
+
+
+  // return the difference between the chainWork at
+  // the blockchain Tip and its 10th ancestor
+  //
+  // this is not needed by the relay itself, but is provided in
+  // case some contract wants to use the chainWork or Bitcoin network
+  // difficulty (which can be derived) as a data feed for some purpose
+  function getAverageChainWork() returns (uint) {
+      uint blockHash = bestBlockHash;
+
+      uint128 chainWorkTip = m_getScore(blockHash);
+
+      uint8 i = 0;
+      while (i < 10) {
+          blockHash = getPrevBlock(blockHash);
+          i += 1;
+      }
+
+      uint128 chainWork10Ancestors = m_getScore(blockHash);
+
+      return (chainWorkTip - chainWork10Ancestors);
+  }
+
 
 }
