1d5c1d1582ac753b694e5912fbfd3bf17db40208
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2017-12-18 17:57:26 -0300

    Add function to calculate ethereum address from the bitcoin public key

--- contracts/token/DogeToken.sol
+++ contracts/token/DogeToken.sol
@@ -42,6 +42,55 @@ contract DogeToken is HumanStandardToken(0, "DogeToken", 8, "DOGETOKEN"), Transa
         return 1;
     }
 
+    uint constant p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f;  // secp256k1
+    uint constant q = (p + 1) / 4;
+
+    function getAddress(bytes pubKey) view internal returns (address) {
+        uint x;
+        bool odd;
+        require(pubKey.length == 33);
+        require(pubKey[0] == 2 || pubKey[0] == 3);
+        odd = pubKey[0] == 3;
+        assembly {
+            x := mload(add(pubKey, 33))
+        }
+        //FIXME: Check Legendre operator to ensure x is valid
+        return pub2address(x, odd);
+    }
+
+    function expmod(uint256 base, uint256 e, uint256 m) internal constant returns (uint256 o) {
+        // are all of these inside the precompile now?
+
+        assembly {
+            // define pointer
+            let p := mload(0x40)
+            // store data assembly-favouring ways
+            mstore(p, 0x20)             // Length of Base
+            mstore(add(p, 0x20), 0x20)  // Length of Exponent
+            mstore(add(p, 0x40), 0x20)  // Length of Modulus
+            mstore(add(p, 0x60), base)  // Base
+            mstore(add(p, 0x80), e)     // Exponent
+            mstore(add(p, 0xa0), m)     // Modulus
+            // call modexp precompile!
+            if iszero(call(not(0), 0x05, 0, p, 0xc0, p, 0x20)) {
+                revert(0, 0)
+            }
+            // data
+            o := mload(p)
+        }
+    }
+
+    function pub2address(uint x, bool odd) internal returns (address) {
+        uint yy = mulmod(x, x, p);
+        yy = mulmod(yy, x, p);
+        yy = addmod(yy, 7, p);
+        uint y = expmod(yy, q, p);
+        if (((y & 1) == 1) != odd) {
+          y = p - y;
+        }
+        return address(keccak256(x, y));
+    }
+
     struct DogeTransaction {
     }
 
