38c2d2349d73049e77192ac4fe4f7c9076e1af58
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-01-02 14:34:52 -0300

    Update debug script

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,5 +1,6 @@
 var DogeRelayForTests = artifacts.require("./DogeRelayForTests.sol");
 var DogeToken = artifacts.require("./token/DogeToken.sol");
+var utils = require('../test/utils');
 
 module.exports = function(callback) {
   var dr;
@@ -11,6 +12,13 @@ module.exports = function(callback) {
     return dr.getBestBlockHeight.call(); 
   }).then(function(result) {
     console.log("Best block height : " + result.toString(10));
+    return dr.getBlockLocator.call();     
+  }).then(function(result) {
+    var formattedResult = new Array();
+    result.forEach(function(element) {
+      formattedResult.push(utils.formatHexUint32(element.toString(16)));
+    });    
+    console.log("Locator : " + formattedResult);
     callback();
   }).catch(function(e) {
     // There was an error! Handle it.
