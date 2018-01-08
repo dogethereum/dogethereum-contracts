996ba973c15ac88767829f75139d79d430f3afdd
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2018-01-08 14:04:43 -0300

    Force ropsten gas limit for truffle development configuration

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-      , gas: 4712388000
+      , gas: 4700000
       , gasPrice: 1
     }
   }
