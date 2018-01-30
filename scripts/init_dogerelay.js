27335882c70cc6b29a5f9c3b27120c9fbada6160
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-01-30 12:17:09 -0300

    Set new initial parent

--- scripts/init_dogerelay.js
+++ scripts/init_dogerelay.js
@@ -5,11 +5,8 @@ module.exports = function(callback) {
   var dr;
   DogeRelay.deployed().then(function(instance) {      
     dr = instance;
-    //return dr.setInitialParent(block974400Hash, 974400, 1, {from: accounts[0]}); 
-    //var block2020000Hash = "0x9c1cdf5d0ce3676dc0551a81215d9fed0e45e12f3f7e5b18372a81130b963a2e";  
-    //return dr.setInitialParent(block2020000Hash, 2020000, 1); 
-    var block2054935Hash = "0xde913ba2dba274617193a336fc3a96586b7433bf0390916a59409cf7203bf73b";  
-    return dr.setInitialParent(block2054935Hash, 2054935, 1, {gas: 100000});     
+    var block2075755Hash = "0x972693d9d6e5046844bf1c02b675df183a1fb737a22a1d45e8d213aedcc2d71b";  
+    return dr.setInitialParent(block2075755Hash, 2075755, 1, {gas: 100000});     
   }).then(function(result) {
     // If this callback is called, the transaction was successfully processed.
     console.log("Transaction successful!");
