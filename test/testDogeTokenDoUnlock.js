7c125088c9f57ef3b37187febc52f0a0658ea78d
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-03-06 22:53:25 -0300

    Code clean up

--- test/testDogeTokenDoUnlock.js
+++ test/testDogeTokenDoUnlock.js
@@ -13,14 +13,12 @@ contract('testDogeTokenDoUnlock', function(accounts) {
     await dogeToken.assign(accounts[0], 2000000000);
     var balance = await dogeToken.balanceOf(accounts[0]);
     assert.equal(balance, 2000000000, `DogeToken's ${accounts[0]} balance is not the expected one`);
-    //await dogeToken.assign(accounts[1], 200000000);
 
     await dogeToken.addUtxo(2000000000, 1, 10);
     const utxo = await dogeToken.utxos(0);
     assert.equal(utxo[0].toNumber(), 2000000000, `Utxo value is not the expected one`);
 
     const dogeAddress = "DHx8ZyJJuiFM5xAHFypfz1k6bd2X85xNMy";
-    //await dogeToken.doUnlock(dogeAddress, 1000000000);
     await dogeToken.doUnlock(dogeAddress, 1000000000).then(function(result) {
       //console.log(result.receipt.logs);
     });
