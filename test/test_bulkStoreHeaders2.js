284cba6a582ed4702d32e0b4818d4cc884c9c1d7
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2018-03-09 14:03:29 -0300

    Update tests to use consistent formatting, async/await

--- test/test_bulkStoreHeaders2.js
+++ test/test_bulkStoreHeaders2.js
@@ -10,27 +10,20 @@ contract('test_bulkStoreHeaders2', function(accounts) {
         const scryptChecker = await ScryptCheckerDummy.new(dr.address, true);
         await dr.setScryptChecker(scryptChecker.address);
     });
-    it("testDifficulty", function() {
+    it("testDifficulty", async () => {
         var headers = "0x";
         var hashes = "0x";
         // Dogecoin testnet
         var block739200Hash = "0x69404b833f190a1a29c32265ad73ea344ccaba82367be0d43d58a9eed2b8d357";
-        return dr.setInitialParent(block739200Hash, 739200, 1, {from: accounts[0]}).then(function(instance) {
-            return utils.parseDataFile('test/headers/elevenDogeTestnet.txt');
-        }).then(function ({ headers: rawHeaders, hashes: rawHashes }) {
-            headers += rawHeaders.map(utils.addSizeToHeader).join('');
-            hashes += rawHeaders.map(utils.calcHeaderPoW).join('');
-            return dr.bulkStoreHeaders(headers, hashes, 11, accounts[2], {from: accounts[0]});
-        }).then(function(result) {
-            //console.log(result.receipt.logs);
-            return dr.getChainWork.call();
-        }).then(function(result) {
-            assert.equal(result.toNumber(), 11 + 1, "difficulty is not the expected one"); // # +1 since setInitialParent was called with imaginary block
-            return dr.getAverageChainWork.call();
-        }).then(function(result) {
-            assert.equal(result.toNumber(), 10, "average chain work is not the expected one");
-        });
+        await dr.setInitialParent(block739200Hash, 739200, 1, {from: accounts[0]});
+        const { headers: rawHeaders, hashes: rawHashes } = await utils.parseDataFile('test/headers/elevenDogeTestnet.txt');
+        headers += rawHeaders.map(utils.addSizeToHeader).join('');
+        hashes += rawHeaders.map(utils.calcHeaderPoW).join('');
+        const result = await dr.bulkStoreHeaders(headers, hashes, 11, accounts[2], {from: accounts[0]});
+        //console.log(result.receipt.logs);
+        const chainWork = await dr.getChainWork.call();
+        assert.equal(chainWork.toNumber(), 11 + 1, "difficulty is not the expected one"); // # +1 since setInitialParent was called with imaginary block
+        const averageChainWork = await dr.getAverageChainWork.call();
+        assert.equal(averageChainWork.toNumber(), 10, "average chain work is not the expected one");
     });
-
-
 });
