519d34db1949e10e568055bcac869c3b38efb523
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2018-03-13 17:44:29 -0300

    Refactor deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,59 +1,60 @@
-var DogeRelay = artifacts.require("./DogeRelay.sol");
-var DogeRelayForTests = artifacts.require("./DogeRelayForTests.sol");
-var DogeProcessor = artifacts.require("./DogeProcessor.sol");
-var Set = artifacts.require("./token/Set.sol");
-var DogeToken = artifacts.require("./token/DogeToken.sol");
-var DogeTokenForTests = artifacts.require("./token/DogeTokenForTests.sol");
-var DogeTx = artifacts.require("./DogeParser/DogeTx.sol");
-var ScryptCheckerDummy = artifacts.require("./ScryptCheckerDummy.sol");
+const DogeRelay = artifacts.require('DogeRelay');
+const DogeRelayForTests = artifacts.require('DogeRelayForTests');
+const DogeProcessor = artifacts.require('DogeProcessor');
+const Set = artifacts.require('token/Set');
+const DogeToken = artifacts.require('token/DogeToken');
+const DogeTokenForTests = artifacts.require('token/DogeTokenForTests');
+const DogeTx = artifacts.require('DogeTx');
+const ScryptCheckerDummy = artifacts.require('ScryptCheckerDummy');
 
 const scryptCheckerAddress = '0xfeedbeeffeedbeeffeedbeeffeedbeeffeedbeef';
 const dogethereumRecipientUnitTest = '0x4d905b4b815d483cdfabcd292c6f86509d0fad82';
 const dogethereumRecipientIntegrationDogeMain = '0x0000000000000000000000000000000000000003';
 const dogethereumRecipientIntegrationDogeRegtest = '0x0000000000000000000000000000000000000004';
 
-module.exports = function(deployer, network, accounts) {
-  var dogethereumRecipient;
-  if (network === 'development') {
-    dogethereumRecipient = dogethereumRecipientUnitTest;
-  } else if (network === 'integrationDogeMain') {
-    dogethereumRecipient = dogethereumRecipientIntegrationDogeMain;
-  } else {
-    dogethereumRecipient = dogethereumRecipientIntegrationDogeRegtest;    
-  }
-  var dogeRelayNetwork;
-  if (network === 'integrationDogeRegtest') {
-    dogeRelayNetwork = 2; // REGTEST
-  } else {
-    dogeRelayNetwork = 0; // MAIN
-  }
+const DOGE_MAINNET = 0;
+const DOGE_REGTEST = 2;
 
-  deployer.deploy(Set, {gas: 300000});
-  deployer.deploy(DogeTx, {gas: 100000});
-  if (network === 'development') {
-    deployer.link(Set, DogeTokenForTests);
-    deployer.link(DogeTx, DogeTokenForTests);
-    return deployer.deploy(DogeRelayForTests, dogeRelayNetwork, {gas: 4600000}).then(function () {
-      return deployer.deploy(ScryptCheckerDummy, DogeRelayForTests.address, true, {gas: 1000000})
-    }).then(function () {
-      return deployer.deploy(DogeProcessor, DogeRelayForTests.address, {gas: 3600000});
-    }).then(function () {
-      return deployer.deploy(DogeTokenForTests, DogeRelayForTests.address, dogethereumRecipient, {gas: 4400000});
-    }).then(function () {
-      const dogeRelay = DogeRelayForTests.at(DogeRelayForTests.address);
-      return dogeRelay.setScryptChecker(ScryptCheckerDummy.address, {gas: 1000000});
-    });
-  } else {
-    deployer.link(Set, DogeToken);
-    deployer.link(DogeTx, DogeToken);
-    return deployer.deploy(DogeRelay, dogeRelayNetwork, {gas: 3600000}).then(function () {
-      return deployer.deploy(DogeToken, DogeRelay.address, dogethereumRecipient, {gas: 4200000});
-    }).then(function () {
-      return deployer.deploy(ScryptCheckerDummy, DogeRelay.address, true, {gas: 1000000})
-    }).then(function () {
-      const dogeRelay = DogeRelay.at(DogeRelay.address);
-      //return dogeRelay.setScryptChecker(scryptCheckerAddress, {gas: 100000});
-      return dogeRelay.setScryptChecker(ScryptCheckerDummy.address, {gas: 100000});
-    });
-  }
+async function deployDevelopment(deployer, network, accounts, networkId, dogethereumRecipient) {
+  await deployer.deploy(Set);
+  await deployer.deploy(DogeTx);
+
+  await deployer.link(Set, DogeTokenForTests);
+  await deployer.link(DogeTx, DogeTokenForTests);
+
+  await deployer.deploy(DogeRelayForTests, networkId);
+  await deployer.deploy(ScryptCheckerDummy, DogeRelayForTests.address, true)
+  await deployer.deploy(DogeProcessor, DogeRelayForTests.address);
+  await deployer.deploy(DogeTokenForTests, DogeRelayForTests.address, dogethereumRecipient);
+
+  const dogeRelay = DogeRelayForTests.at(DogeRelayForTests.address);
+  await dogeRelay.setScryptChecker(ScryptCheckerDummy.address);
+}
+
+async function deployProduction(deployer, network, accounts, networkId, dogethereumRecipient) {
+  await deployer.deploy(Set);
+  await deployer.deploy(DogeTx);
+
+  await deployer.link(Set, DogeToken);
+  await deployer.link(DogeTx, DogeToken);
+
+  await deployer.deploy(DogeRelay, networkId);
+  await deployer.deploy(DogeToken, DogeRelay.address, dogethereumRecipient);
+
+  await deployer.deploy(ScryptCheckerDummy, DogeRelay.address, true)
+
+  const dogeRelay = DogeRelay.at(DogeRelay.address);
+  await dogeRelay.setScryptChecker(ScryptCheckerDummy.address);
+}
+
+module.exports = function(deployer, network, accounts) {
+  deployer.then(async () => {
+    if (network === 'development' || network === 'ropsten') {
+      await deployDevelopment(deployer, network, accounts, DOGE_MAINNET, dogethereumRecipientUnitTest);
+    } else if (network === 'integrationDogeMain') {
+      await deployProduction(deployer, network, accounts, DOGE_MAINNET, dogethereumRecipientIntegrationDogeMain);
+    } else if (network === 'integrationDogeRegtest') {
+      await deployProduction(deployer, network, accounts, DOGE_REGTEST, dogethereumRecipientIntegrationDogeRegtest);
+    }
+  });
 };
