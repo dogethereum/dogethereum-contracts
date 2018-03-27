258fcb79ffbcb103bd54a4c0471d1c549151cb4d
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-03-27 16:07:55 -0300

    debug.js : use async/await

--- scripts/debug.js
+++ scripts/debug.js
@@ -2,40 +2,23 @@ var DogeRelay = artifacts.require("./DogeRelay.sol");
 var DogeToken = artifacts.require("./token/DogeToken.sol");
 var utils = require('../test/utils');
 
-module.exports = function(callback) {
-  var dr;
-  var dt;  
-  DogeRelay.deployed().then(function(instance) {      
-    dr = instance;
-    return dr.getBestBlockHash.call(); 
-  }).then(function(result) {
-    console.log("Best block hash : " + result.toString(16));
-    return dr.getBestBlockHeight.call(); 
-  }).then(function(result) {
-    console.log("Best block height : " + result.toString(10));
-    return dr.getBlockLocator.call();     
-  }).then(function(result) {
-    var formattedResult = new Array();
-    result.forEach(function(element) {
-      formattedResult.push(utils.formatHexUint32(element.toString(16)));
-    });    
-    console.log("Locator : " + formattedResult);
-    return DogeToken.deployed();
-  }).then(function(instance) {
-    dt = instance;
-    return dt.balanceOf.call("0x92ecc1ba4ea10f681dcf35c02f583e59d2b99b4b"); 
-  }).then(function(result) {
-    console.log("Balance of 0x92ecc1ba4ea10f681dcf35c02f583e59d2b99b4b : " + result);
-    return dt.balanceOf.call("0xd2394f3fad76167e7583a876c292c86ed10305da"); 
-  }).then(function(result) {
-    console.log("Balance of 0xd2394f3fad76167e7583a876c292c86ed10305da : " + result);
-    return dt.dogeEthPrice.call(); 
-  }).then(function(result) {
-    console.log("Doge-Eth price : " + result);
-    callback();
-  }).catch(function(e) {
-    // There was an error! Handle it.
-    console.log(e);
-    callback(e);
-  });
+module.exports = async function(callback) {
+  var dr  = await DogeRelay.deployed();
+  var bestBlockHash = await dr.getBestBlockHash.call(); 
+  console.log("Best block hash : " + bestBlockHash.toString(16));
+  var bestBlockHeight = await dr.getBestBlockHeight.call(); 
+  console.log("Best block height : " + bestBlockHeight.toString(10));
+  var blockLocator = await dr.getBlockLocator.call();     
+  var blockLocatorFormatted = new Array();
+  blockLocator.forEach(function(element) {
+      blockLocatorFormatted.push(utils.formatHexUint32(element.toString(16)));
+  });    
+  console.log("Locator : " + blockLocatorFormatted);
+  var dt = await DogeToken.deployed();
+  var balance1 = await dt.balanceOf.call("0x92ecc1ba4ea10f681dcf35c02f583e59d2b99b4b"); 
+  console.log("Balance of 0x92ecc1ba4ea10f681dcf35c02f583e59d2b99b4b : " + balance1);
+  var balance2 = await dt.balanceOf.call("0xd2394f3fad76167e7583a876c292c86ed10305da"); 
+  console.log("Balance of 0xd2394f3fad76167e7583a876c292c86ed10305da : " + balance2);
+  var dogeEthPrice = await dt.dogeEthPrice.call(); 
+  console.log("Doge-Eth price : " + dogeEthPrice);
 }
