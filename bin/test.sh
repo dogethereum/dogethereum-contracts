5aeaeee7622ceaf403393047e9ca34e878cff1d1
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-04-10 13:07:33 -0300

    Add travis integration

---
+++ bin/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+ganache-cli --gasLimit <yourLimit> 2> /dev/null 1> /dev/null &
+sleep 5 # to make sure ganache-cli is up and running before compiling
+rm -rf build
+truffle compile
+truffle migrate --reset --network development
+truffle test
+sleep n
+kill -9 $(lsof -t -i:8545)
