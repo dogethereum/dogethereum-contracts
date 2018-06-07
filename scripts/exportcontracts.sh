3c7888f7f1bee032575af10609df59fc01e2eeee
cat-j <catalinajuarros@gmail.com>
2018-06-07 11:35:24 -0300

    Export script for local copy, might remove later

--- scripts/exportcontracts.sh
+++ scripts/exportcontracts.sh
@@ -4,7 +4,7 @@ rm -rf build
 truffle compile
 truffle migrate --reset --network integrationDogeRegtest
 
-/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogerelay/build/contracts/DogeRelay.json -o ~/dogesubmitter/src/main/java/ -p org.dogethereum.dogesubmitter.contract
-/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogerelay/build/contracts/DogeToken.json -o ~/dogesubmitter/src/main/java/ -p org.dogethereum.dogesubmitter.contract
-/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogerelay/build/contracts/ClaimManager.json -o ~/dogesubmitter/src/main/java/ -p org.dogethereum.dogesubmitter.contract
-/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogerelay/build/contracts/Superblocks.json -o ~/dogesubmitter/src/main/java/ -p org.dogethereum.dogesubmitter.contract
+/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogerelay/build/contracts/DogeRelay.json -o ~/agent/src/main/java/ -p org.dogethereum.dogesubmitter.contract
+/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogerelay/build/contracts/DogeToken.json -o ~/agent/src/main/java/ -p org.dogethereum.dogesubmitter.contract
+/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogerelay/build/contracts/ClaimManager.json -o ~/agent/src/main/java/ -p org.dogethereum.dogesubmitter.contract
+/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogerelay/build/contracts/Superblocks.json -o ~/agent/src/main/java/ -p org.dogethereum.dogesubmitter.contract
