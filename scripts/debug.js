0225f39788dbf694ee7762a63662aa2a310d4e67
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-06-22 18:59:40 -0300

    Fix debug.js

--- scripts/debug.js
+++ scripts/debug.js
@@ -7,9 +7,9 @@ module.exports = async function(callback) {
   console.log("DogeRelay");
   console.log("---------");
   var bestBlockHash = await dr.getBestBlockHash.call(); 
-  console.log("Best block hash : " + bestBlockHash.toString(16));
+  console.log("Doge Best block hash : " + bestBlockHash.toString(16));
   var bestBlockHeight = await dr.getBestBlockHeight.call(); 
-  console.log("Best block height : " + bestBlockHeight.toString(10));
+  console.log("Doge Best block height : " + bestBlockHeight.toString(10));
   var blockLocator = await dr.getBlockLocator.call();     
   var blockLocatorFormatted = new Array();
   blockLocator.forEach(function(element) {
@@ -24,6 +24,10 @@ module.exports = async function(callback) {
   console.log("DogeToken Balance of 0x92ecc1ba4ea10f681dcf35c02f583e59d2b99b4b : " + balance1);
   var balance2 = await dt.balanceOf.call("0xd2394f3fad76167e7583a876c292c86ed10305da"); 
   console.log("DogeToken Balance of 0xd2394f3fad76167e7583a876c292c86ed10305da : " + balance2);
+  var balance3 = await dt.balanceOf.call("0xf5fa014271b7971cb0ae960d445db3cb3802dfd9"); 
+  console.log("DogeToken Balance of 0xf5fa014271b7971cb0ae960d445db3cb3802dfd9 : " + balance3);
+
+
   var dogeEthPrice = await dt.dogeEthPrice.call(); 
   console.log("Doge-Eth price : " + dogeEthPrice);
 
@@ -52,7 +56,7 @@ module.exports = async function(callback) {
   }
  
   // Current block number 
-  console.log("Current block : " + web3.eth.blockNumber);
+  console.log("Eth Current block : " + web3.eth.blockNumber);
 
   // Unlock events
   var unlockRequestEvent = dt.UnlockRequest({}, {fromBlock: 0, toBlock: "latest"});
