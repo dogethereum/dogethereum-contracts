80662eb3ea83c363cfd1113c48e056f3c0d7de12
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2018-07-18 11:23:32 -0300

    Remove commented code

--- contracts/DogeParser/DogeTxForTests.sol
+++ contracts/DogeParser/DogeTxForTests.sol
@@ -17,64 +17,6 @@ contract DogeTxForTests {
         return DogeTx.sliceArray(original, offset, endIndex);
     }
 
-    //function getBlockHash(uint blockHeight) public view returns (uint) {
-    //    return fastGetBlockHash(blockHeight);
-    //}
-
-
-    // return the chainWork of the Tip
-    // http://bitcoin.stackexchange.com/questions/26869/what-is-chainwork
-    //function getChainWork() public view returns (uint128) {
-    //    return getScore(bestBlockHash);
-    //}
-
-
-    // return the difference between the chainWork at
-    // the blockchain Tip and its 10th ancestor
-    //
-    // this is not needed by the relay itself, but is provided in
-    // case some contract wants to use the chainWork or Bitcoin network
-    // difficulty (which can be derived) as a data feed for some purpose
-    //function getAverageChainWork() public view returns (uint) {
-    //    uint blockHash = bestBlockHash;
-
-    //    uint128 chainWorkTip = getScore(blockHash);
-
-    //    uint8 i = 0;
-    //    while (i < 10) {
-    //        blockHash = getPrevBlock(blockHash);
-    //        i += 1;
-    //    }
-
-        // uint128 chainWork10Ancestors = getScore(blockHash);
-
-    //    return (chainWorkTip - getScore(blockHash));
-    //}
-
-    // returns the block header (zeros for a header that does not exist) when
-    // sufficient payment is provided.  If payment is insufficient, returns 1-byte of zero.
-    //function getBlockHeader(uint blockHash) internal returns (DogeTx.BlockHeader) {
-    //    // TODO: incentives
-    //    // if (feePaid(blockHash, m_getFeeAmount(blockHash))) {  // in incentive.se
-    //    //     GetHeader (blockHash, 0);
-    //    //    return(text("\x00"):str);
-    //    // }
-    //    emit GetHeader(bytes32(blockHash), 1);
-    //    return myblocks[blockHash]._blockHeader;
-    //}
-
-    //function getPrevBlockPublic(uint blockHash) public view returns (uint) {
-    //    return getPrevBlock(blockHash);
-    //}
-
-    //function getTimestampPublic(uint blockHash) public view returns (uint32 result) {
-    //    return getTimestamp(blockHash);
-    //}
-
-    //function getBitsPublic(uint blockHash) public view returns (uint32 result) {
-    //    return getBits(blockHash);
-    //}
-
     function targetFromBitsPublic(uint32 bits) public pure returns (uint) {
         return DogeTx.targetFromBits(bits) ;
     }
