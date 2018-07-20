239a90cb6b155f768f6f450bb98eba16568c3d02
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-07-20 16:04:06 -0300

    init_dogerelayXXX.js fixes

--- scripts/init_dogerelay.js
+++ scripts/init_dogerelay.js
@@ -2,9 +2,6 @@ var DogeRelay = artifacts.require("./DogeRelay.sol");
 
 module.exports = async function(callback) {
   console.log("init_dogerelay begin");
-  // Set DogeRelay inital parent 
-  var dr = await DogeRelay.deployed();
-  var block2079057Hash = "0x56ce90c0c12a737c500d67c17663d83c35242e32fafcd1edc9c5b6b9aee2d2c1";  
-  var result = await dr.setInitialParent(block2079057Hash, 2079057, 1, {gas: 100000});     
+  // TODO implement this
   console.log("init_dogerelay end");
 }
--- scripts/init_dogerelay_ethganachedogemain.js
+++ scripts/init_dogerelay_ethganachedogemain.js
@@ -3,7 +3,7 @@ var Superblocks = artifacts.require("./DogeSuperblocks.sol");
 const utils = require('./../test/utils');
 
 module.exports = async function(callback) {
-  console.log("init_dogerelay_regtest begin");
+  console.log("init_dogerelay_ethganachedogemain begin");
   
   // Calculate operator public key
   const operatorPublicKeyHash = '0x03cd041b0139d3240607b9fd1b2d1b691e22b5d6';
@@ -19,15 +19,22 @@ module.exports = async function(callback) {
   await dt.addOperatorDeposit(operatorPublicKeyHash, {value: 1000000000000000000, from : operatorEthAddress});
 
   var sb = await Superblocks.deployed();
-  var blocksMerkleRoot = "0x3d2160a3b5dc4a9d62e7e66a295f70313ac808440ef7400d6c0772171ce973a5";
-  var accumulatedWork = web3.toBigNumber("0");
-  var timestamp = 1296688602;
-  var prevTimestamp = 0;
-  var lastHash = "0x3d2160a3b5dc4a9d62e7e66a295f70313ac808440ef7400d6c0772171ce973a5";
-  var lastBits = 0x207fffff;
+  // blocks from dogemain-2299155-to-2299210 
+  //var blocksMerkleRoot = "0x49548a60f34bef021845d5d6a8485f276ed89d391ed6779e9e4cbdf3bd2d39e5";
+  //var accumulatedWork = web3.toBigNumber("3294865331135006206033");
+  //var timestamp = 1531295965;
+  //var prevTimestamp = 1531295930;
+  //var lastHash = "0xc577a73270eb1fccd4a702402089f653c771749763e0d7ebb877f47e81eb4395";
+  //var lastBits = 436591711;
+  // blocks from dogemain-2309215-to-2309216
+  var blocksMerkleRoot = "0x1eb62592c39990b4d33b55eac0989ec9ad69099aced17b8adc56ed561b28b473";
+  var accumulatedWork = web3.toBigNumber("3434911961284113526919");
+  var timestamp = 1531922574;
+  var prevTimestamp = 1531922557;
+  var lastHash = "0x046722472396fe2883a725f97f0e63036d2064ceb271bccc175578b724833b3f";
+  var lastBits = 436643408;
   var parentId = "0x0";
   await sb.initialize(blocksMerkleRoot, accumulatedWork, timestamp, prevTimestamp, lastHash, lastBits, parentId);
 
-  console.log("init_dogerelay_regtest end");
-
+  console.log("init_dogerelay_ethganachedogemain end");
 }
