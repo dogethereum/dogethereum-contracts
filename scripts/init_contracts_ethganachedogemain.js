5a588fac6250514baac7ca3b01bd3a397ff04dc7
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-08-07 19:08:27 -0300

    Update genesis superblock for doge mainnet

--- scripts/init_contracts_ethganachedogemain.js
+++ scripts/init_contracts_ethganachedogemain.js
@@ -19,29 +19,13 @@ module.exports = async function(callback) {
   await dt.addOperatorDeposit(operatorPublicKeyHash, {value: 1000000000000000000, from : operatorEthAddress});
 
   var sb = await Superblocks.deployed();
-  // blocks from dogemain-2299155-to-2299210 
-  //var blocksMerkleRoot = "0x49548a60f34bef021845d5d6a8485f276ed89d391ed6779e9e4cbdf3bd2d39e5";
-  //var accumulatedWork = web3.toBigNumber("3294865331135006206033");
-  //var timestamp = 1531295965;
-  //var prevTimestamp = 1531295930;
-  //var lastHash = "0xc577a73270eb1fccd4a702402089f653c771749763e0d7ebb877f47e81eb4395";
-  //var lastBits = 436591711;
 
-  // blocks from dogemain-2309215-to-2309216
-  // var blocksMerkleRoot = "0x1eb62592c39990b4d33b55eac0989ec9ad69099aced17b8adc56ed561b28b473";
-  // var accumulatedWork = web3.toBigNumber("3434911961284113526919");
-  // var timestamp = 1531922574;
-  // var prevTimestamp = 1531922557;
-  // var lastHash = "0x046722472396fe2883a725f97f0e63036d2064ceb271bccc175578b724833b3f";
-  // var lastBits = 436643408;
-
-  // blocks from dogemain-2331583-to-2331588
-  var blocksMerkleRoot = "0xb56e1308bc44483551a5d3ba426d83c2b7634b6c985a3128e48544e8d0fe4ec2";
-  var accumulatedWork = web3.toBigNumber("3752336556886305017875");
-  var timestamp = 1533320063;
-  var prevTimestamp = 1533320029;
-  var lastHash = "0xb0dbc74bc6e258e882a527206d67579cc231be743b23500a28015c96d66ed05a";
-  var lastBits = 436473103;
+  var blocksMerkleRoot = "0xd6ba2a22aae9bbe860f7df8be83cc05dfd584121aaa1868a1926a8acec3fecb3";
+  var accumulatedWork = web3.toBigNumber("3832331074689355151779");
+  var timestamp = 1533678578;
+  var prevTimestamp = 1533678523;
+  var lastHash = "0xc2c204e82ff21092797bf451acaaf9a2074bd650e2f0dca366cca7dd0cd94d9e";
+  var lastBits = 436541183;
   var parentId = "0x0";
 
   await sb.initialize(blocksMerkleRoot, accumulatedWork, timestamp, prevTimestamp, lastHash, lastBits, parentId);
--- scripts/init_contracts_integration.js
+++ scripts/init_contracts_integration.js
@@ -3,13 +3,13 @@ module.exports = async function(callback) {
   console.log("init_contracts_integration begin");
   
   var sb = await Superblocks.deployed();
-  // blocks from dogemain-2331583-to-2331588
-  var blocksMerkleRoot = "0xb56e1308bc44483551a5d3ba426d83c2b7634b6c985a3128e48544e8d0fe4ec2";
-  var accumulatedWork = web3.toBigNumber("3752336556886305017875");
-  var timestamp = 1533320063;
-  var prevTimestamp = 1533320029;
-  var lastHash = "0xb0dbc74bc6e258e882a527206d67579cc231be743b23500a28015c96d66ed05a";
-  var lastBits = 436473103;
+
+  var blocksMerkleRoot = "0xd6ba2a22aae9bbe860f7df8be83cc05dfd584121aaa1868a1926a8acec3fecb3";
+  var accumulatedWork = web3.toBigNumber("3832331074689355151779");
+  var timestamp = 1533678578;
+  var prevTimestamp = 1533678523;
+  var lastHash = "0xc2c204e82ff21092797bf451acaaf9a2074bd650e2f0dca366cca7dd0cd94d9e";
+  var lastBits = 436541183;
   var parentId = "0x0";
 
   await sb.initialize(blocksMerkleRoot, accumulatedWork, timestamp, prevTimestamp, lastHash, lastBits, parentId);
