23c5b0d1197a3d9c5f0fe047959b6d7ace644074
cat-j <catalinajuarros@gmail.com>
2018-08-10 11:15:48 -0300

    Don't export DogeBattleManager

--- scripts/exportcontracts.sh
+++ scripts/exportcontracts.sh
@@ -7,4 +7,3 @@ truffle migrate --reset --network integrationDogeRegtest
 /home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogethereum-contracts/build/contracts/DogeToken.json -o ~/agents/src/main/java/ -p org.dogethereum.agents.contract
 /home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogethereum-contracts/build/contracts/DogeClaimManager.json -o ~/agents/src/main/java/ -p org.dogethereum.agents.contract
 /home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogethereum-contracts/build/contracts/DogeSuperblocks.json -o ~/agents/src/main/java/ -p org.dogethereum.agents.contract
-/home/cat/software/web3j-3.3.1/bin/web3j truffle generate ~/dogethereum-contracts/build/contracts/DogeBattleManager.json -o ~/agents/src/main/java/ -p org.dogethereum.agents.contract
