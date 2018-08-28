c6ee9b3517cc134994caedb149b0a5b0c8268249
cat-j <catalinajuarros@gmail.com>
2018-08-28 12:30:22 -0300

    Challenge one descendant

--- test/approveDescendant.js
+++ test/approveDescendant.js
@@ -47,19 +47,14 @@ contract('approveDescendant', (accounts) => {
         `03006200d59a7170a948a2e0b58c31b5cccb79b37686ca0b5a460d65e75273f9bc0d90deb0bbce23de660ab3f1e1b21b6e22919d8cf5e2f6b1de28e11611049f824503a1774d7c5bffff7f20010000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff03580101ffffffff0100203d88792d00002321032af59b4b1ee8905f7f0a05b21e4b9ee2544426943166fb5b6514eb60c9764d2fac00000000`,
         `03006200c62f797c15dd491c2bef442d0ba509fc98d47e9ab644ed3809fcabec58ba542f09e6142339f1e34aad3c95f593d66c9fc65f19b3319ec3d8363b9f8c695eab8c774d7c5bffff7f20020000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff03590101ffffffff0100203d88792d00002321032af59b4b1ee8905f7f0a05b21e4b9ee2544426943166fb5b6514eb60c9764d2fac00000000`
     ];
-    const superblock4Headers = [
-        `0300620046cd8831add01fdbfecff0cfaa4df27a50d31e7698f8f72ad11fbe796cf5bb6a646b600c2c0a43db62a71b476d7cf5fc5fe031b441882251dc0c4358d6e748a347e37e5bffff7f20030000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff035a0101ffffffff0100203d88792d00002321032af59b4b1ee8905f7f0a05b21e4b9ee2544426943166fb5b6514eb60c9764d2fac00000000`,
-        `0300620062e0af0dfea7f405baf7b93caffe7a2dcf0a1168e5756695cbf8c40771d1621a4f016681e909ef55460b29dd99d153216fbddd56c02398720034e51c5ba452e347e37e5bffff7f20020000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff035b0101ffffffff0100203d88792d00002321032af59b4b1ee8905f7f0a05b21e4b9ee2544426943166fb5b6514eb60c9764d2fac00000000`,
-        `030062002b026da0023e871a35b4a45eefcd24c17994a1ecea1117e24eb405da31c07ab6c338737444918707357e77c3eab47858a8359c7e037c98caa8a37591d546cdde47e37e5bffff7f20000000000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff035c0101ffffffff0100203d88792d00002321032af59b4b1ee8905f7f0a05b21e4b9ee2544426943166fb5b6514eb60c9764d2fac00000000`
-    ]
     
     const superblock1Hashes = superblock1Headers.map(utils.calcBlockSha256Hash);
+    const superblock2Hashes = superblock2Headers.map(utils.calcBlockSha256Hash);
 
     const superblock0 = utils.makeSuperblock(superblock0Headers, initParentId, 2);
     const superblock1 = utils.makeSuperblock(superblock1Headers, superblock0.superblockId, 8);
     const superblock2 = utils.makeSuperblock(superblock2Headers, superblock1.superblockId, 14);
     const superblock3 = utils.makeSuperblock(superblock3Headers, superblock2.superblockId, 20);
-    const superblock4 = utils.makeSuperblock(superblock4Headers, superblock3.superblockId, 26);
 
     // Copied from claimManager.js
     async function initSuperblocks(dummyChecker, genesisSuperblock) {
@@ -84,7 +79,7 @@ contract('approveDescendant', (accounts) => {
             { from: owner },
         );
         //FIXME: ganache-cli creates the same transaction hash if two account send the same amount
-        await claimManager.makeDeposit({ value: 10, from: submitter });
+        await claimManager.makeDeposit({ value: 20, from: submitter });
         await claimManager.makeDeposit({ value: 11, from: challenger });
     }
 
@@ -238,6 +233,193 @@ contract('approveDescendant', (accounts) => {
             
             const bestSuperblock = await superblocks.getBestSuperblock();
             assert.equal(bestSuperblock, superblock3Id, 'Bad best superblock');
+        });
+    });
+
+    describe('Challenged descendant', () => {
+        let superblock0Id;
+        let superblock1Id;
+        let superblock2Id;
+        let superblock3Id;
+        let superblockR0Id;
+
+        let session1;
+
+        before(async () => {
+            await initSuperblocks(true, superblock0);
+        });
+
+        it('Initialized', async () => {
+            superblock0Id = superblock0.superblockId;
+            const best = await superblocks.getBestSuperblock();
+            assert.equal(superblock0Id, best, 'Best superblock should match');
+        });
+
+        // Propose initial superblock
+        it('Propose superblock 1', async () => {
+            const result = await claimManager.proposeSuperblock(
+                superblock1.merkleRoot,
+                superblock1.accumulatedWork,
+                superblock1.timestamp,
+                superblock1.prevTimestamp,
+                superblock1.lastHash,
+                superblock1.lastBits,
+                superblock1.parentId,
+                { from: submitter },
+            );
+            assert.equal(result.logs[1].event, 'SuperblockClaimCreated', 'New superblock proposed');
+            superblock1Id = result.logs[1].args.superblockId;
+        });
+
+        it('Challenge superblock 1', async () => {
+            const result = await claimManager.challengeSuperblock(superblock1Id, { from: challenger });
+            assert.equal(result.logs[1].event, 'SuperblockClaimChallenged', 'Superblock challenged');
+            assert.equal(superblock1Id, result.logs[1].args.claimId);
+            assert.equal(result.logs[2].event, 'NewBattle', 'New battle session');
+            session1 = result.logs[2].args.sessionId;
+            assert.equal(result.logs[3].event, 'VerificationGameStarted', 'Battle started');
+        });
+
+        it('Query and verify hashes', async () => {
+            result = await claimManager.queryMerkleRootHashes(superblock1Id, session1, { from: challenger });
+            assert.equal(result.logs[1].event, 'QueryMerkleRootHashes', 'Query merkle root hashes');
+            result = await claimManager.respondMerkleRootHashes(superblock1Id, session1, superblock1Hashes, { from: submitter });
+            assert.equal(result.logs[1].event, 'RespondMerkleRootHashes', 'Respond merkle root hashes');
+        });
+        
+        it('Query and reply block header', async () => {
+            let scryptHash;
+            result = await claimManager.queryBlockHeader(superblock1Id, session1, superblock1Hashes[0], { from: challenger });
+            assert.equal(result.logs[1].event, 'QueryBlockHeader', 'Query block header');
+            scryptHash = `0x${utils.calcHeaderPoW(superblock1Headers[0])}`;
+            result = await claimManager.respondBlockHeader(superblock1Id, session1, scryptHash, `0x${superblock1Headers[0]}`, { from: submitter });
+            assert.equal(result.logs[1].event, 'RespondBlockHeader', 'Respond block header');
+            result = await claimManager.queryBlockHeader(superblock1Id, session1, superblock1Hashes[1], { from: challenger });
+            assert.equal(result.logs[1].event, 'QueryBlockHeader', 'Query block header');
+            scryptHash = `0x${utils.calcHeaderPoW(superblock1Headers[1])}`;
+            result = await claimManager.respondBlockHeader(superblock1Id, session1, scryptHash, `0x${superblock1Headers[1]}`, { from: submitter });
+            assert.equal(result.logs[1].event, 'RespondBlockHeader', 'Respond block header');
+            result = await claimManager.queryBlockHeader(superblock1Id, session1, superblock1Hashes[2], { from: challenger });
+            assert.equal(result.logs[1].event, 'QueryBlockHeader', 'Query block header');
+            scryptHash = `0x${utils.calcHeaderPoW(superblock1Headers[2])}`;
+            result = await claimManager.respondBlockHeader(superblock1Id, session1, scryptHash, `0x${superblock1Headers[2]}`, { from: submitter });
+            assert.equal(result.logs[1].event, 'RespondBlockHeader', 'Respond block header');
+        });
+        
+        it('Verify superblock 1', async () => {
+            result = await claimManager.verifySuperblock(session1, { from: challenger });
+            assert.equal(result.logs[0].event, 'SuperblockBattleDecided', 'Superblock verified');
+            assert.equal(result.logs[1].event, 'ChallengerConvicted', 'Challenger not convicted despite fork being initially valid');
+        });
+
+        it('Semi-approve superblock 1', async () => {
+            await utils.timeoutSeconds(3*SUPERBLOCK_TIMES_DOGE_REGTEST.TIMEOUT);
+            result = await claimManager.checkClaimFinished(superblock1Id, { from: challenger });
+            assert.equal(result.logs[0].event, 'SuperblockClaimPending', 'Superblock challenged');
+            const status = await superblocks.getSuperblockStatus(superblock1Id);
+            assert.equal(status.toNumber(), SEMI_APPROVED, 'Superblock was not semi-approved');
+        });
+
+        it('Propose superblock 2', async () => {
+            const result = await claimManager.proposeSuperblock(
+                superblock2.merkleRoot,
+                superblock2.accumulatedWork,
+                superblock2.timestamp,
+                superblock2.prevTimestamp,
+                superblock2.lastHash,
+                superblock2.lastBits,
+                superblock2.parentId,
+                { from: submitter },
+            );
+            assert.equal(result.logs[1].event, 'SuperblockClaimCreated', 'New superblock proposed');
+            superblock2Id = result.logs[1].args.superblockId;
+        });
+
+        it('Challenge superblock 2', async () => {
+            const result = await claimManager.challengeSuperblock(superblock2Id, { from: challenger });
+            assert.equal(result.logs[1].event, 'SuperblockClaimChallenged', 'Superblock challenged');
+            assert.equal(superblock2Id, result.logs[1].args.claimId);
+            assert.equal(result.logs[2].event, 'NewBattle', 'New battle session');
+            session1 = result.logs[2].args.sessionId;
+            assert.equal(result.logs[3].event, 'VerificationGameStarted', 'Battle started');
+        });
+
+        it('Query and verify hashes', async () => {
+            result = await claimManager.queryMerkleRootHashes(superblock2Id, session1, { from: challenger });
+            assert.equal(result.logs[1].event, 'QueryMerkleRootHashes', 'Query merkle root hashes');
+            result = await claimManager.respondMerkleRootHashes(superblock2Id, session1, superblock2Hashes, { from: submitter });
+            assert.equal(result.logs[1].event, 'RespondMerkleRootHashes', 'Respond merkle root hashes');
+        });
+        
+        it('Query and reply block header', async () => {
+            let scryptHash;
+            result = await claimManager.queryBlockHeader(superblock2Id, session1, superblock2Hashes[0], { from: challenger });
+            assert.equal(result.logs[1].event, 'QueryBlockHeader', 'Query block header');
+            scryptHash = `0x${utils.calcHeaderPoW(superblock1Headers[0])}`;
+            result = await claimManager.respondBlockHeader(superblock2Id, session1, scryptHash, `0x${superblock2Headers[0]}`, { from: submitter });
+            assert.equal(result.logs[1].event, 'RespondBlockHeader', 'Respond block header');
+            result = await claimManager.queryBlockHeader(superblock2Id, session1, superblock2Hashes[1], { from: challenger });
+            assert.equal(result.logs[1].event, 'QueryBlockHeader', 'Query block header');
+            scryptHash = `0x${utils.calcHeaderPoW(superblock2Headers[1])}`;
+            result = await claimManager.respondBlockHeader(superblock2Id, session1, scryptHash, `0x${superblock2Headers[1]}`, { from: submitter });
+            assert.equal(result.logs[1].event, 'RespondBlockHeader', 'Respond block header');
+            result = await claimManager.queryBlockHeader(superblock2Id, session1, superblock2Hashes[2], { from: challenger });
+            assert.equal(result.logs[1].event, 'QueryBlockHeader', 'Query block header');
+            scryptHash = `0x${utils.calcHeaderPoW(superblock2Headers[2])}`;
+            result = await claimManager.respondBlockHeader(superblock2Id, session1, scryptHash, `0x${superblock2Headers[2]}`, { from: submitter });
+            assert.equal(result.logs[1].event, 'RespondBlockHeader', 'Respond block header');
+        });
+        
+        it('Verify superblock 2', async () => {
+            result = await claimManager.verifySuperblock(session1, { from: challenger });
+            assert.equal(result.logs[0].event, 'SuperblockBattleDecided', 'Superblock verified');
+            assert.equal(result.logs[1].event, 'ChallengerConvicted', 'Challenger not convicted despite fork being initially valid');
+        });
+
+        it('Semi-approve superblock 2', async () => {
+            await utils.timeoutSeconds(3*SUPERBLOCK_TIMES_DOGE_REGTEST.TIMEOUT);
+            result = await claimManager.checkClaimFinished(superblock2Id, { from: challenger });
+            assert.equal(result.logs[0].event, 'SuperblockClaimPending', 'Superblock challenged');
+            const status = await superblocks.getSuperblockStatus(superblock2Id);
+            assert.equal(status.toNumber(), SEMI_APPROVED, 'Superblock was not semi-approved');
+        });
+
+        it('Propose superblock 3', async () => {
+            const result = await claimManager.proposeSuperblock(
+                superblock3.merkleRoot,
+                superblock3.accumulatedWork,
+                superblock3.timestamp,
+                superblock3.prevTimestamp,
+                superblock3.lastHash,
+                superblock3.lastBits,
+                superblock3.parentId,
+                { from: submitter },
+            );
+            assert.equal(result.logs[1].event, 'SuperblockClaimCreated', 'New superblock proposed');
+            superblock3Id = result.logs[1].args.superblockId;
+        });
+
+        it('Semi-approve superblock 3', async () => {
+            await utils.timeoutSeconds(3*SUPERBLOCK_TIMES_DOGE_REGTEST.TIMEOUT);
+            result = await claimManager.checkClaimFinished(superblock3Id, { from: challenger });
+            assert.equal(result.logs[0].event, 'SuperblockClaimPending', 'Superblock challenged');
+            const status = await superblocks.getSuperblockStatus(superblock3Id);
+            assert.equal(status.toNumber(), SEMI_APPROVED, 'Superblock was not semi-approved');
+        });
+
+        it('Do not approve descendants because one of them was challenged', async () => {
+            result = await claimManager.confirmClaim(superblock1Id, superblock3Id, { from: submitter });
+            assert.equal(result.logs[0].event, 'SuperblockClaimSuccessful', 'SuperblockClaimSuccessful event missing');
+
+            const status1 = await superblocks.getSuperblockStatus(superblock1Id);
+            const status2 = await superblocks.getSuperblockStatus(superblock2Id);
+            const status3 = await superblocks.getSuperblockStatus(superblock3Id);
+            assert.equal(status1.toNumber(), APPROVED, 'Superblock 1 was not approved');
+            assert.equal(status2.toNumber(), SEMI_APPROVED, 'Superblock 2 status incorrect');
+            assert.equal(status3.toNumber(), SEMI_APPROVED, 'Superblock 3 status incorrect');
+            
+            const bestSuperblock = await superblocks.getBestSuperblock();
+            assert.equal(bestSuperblock, superblock1Id, 'Bad best superblock');
         });
     });
 });
