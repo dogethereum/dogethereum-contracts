34dfbff8a8a6b06b684383b2ebaae12e82877411
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2018-09-25 15:34:52 -0300

    Make some variable names have more meaning

--- test/testDogeTokenNoOperatorOutput.js
+++ test/testDogeTokenNoOperatorOutput.js
@@ -16,20 +16,21 @@ contract('testDogeTokenNoOperatorOutput', function(accounts) {
     pubKeyHash: 0x1e,
     scriptHash: 0x16,
     wif: 0x9e
-  }
+  };
+
   it("Accept unlock transaction without output for operator", async () => {
-    const keyPair = bitcoin.ECPair.fromWIF('QSRUX7i1WVzFW6vx3i4Qj8iPPQ1tRcuPanMun8BKf8ySc8LsUuKx', DOGECOIN);
-    const keyPair2 = bitcoin.ECPair.fromWIF('QULAK58teBn1Xi4eGo4fKea5oQDPMK4vcnmnivqzgvCPagsWHiyf', DOGECOIN);
+    const operatorKeyPair = bitcoin.ECPair.fromWIF('QSRUX7i1WVzFW6vx3i4Qj8iPPQ1tRcuPanMun8BKf8ySc8LsUuKx', DOGECOIN);
+    const operator2KeyPair = bitcoin.ECPair.fromWIF('QULAK58teBn1Xi4eGo4fKea5oQDPMK4vcnmnivqzgvCPagsWHiyf', DOGECOIN);
 
-    const operatorDogeAddress2 = bitcoin.payments.p2pkh({ pubkey: keyPair2.publicKey, network: DOGECOIN }).address;
+    const operator2DogeAddress = bitcoin.payments.p2pkh({ pubkey: operator2KeyPair.publicKey, network: DOGECOIN }).address;
 
-    const operatorPublicKeyHash = `0x${bitcoin.crypto.ripemd160(bitcoin.crypto.sha256(keyPair.publicKey)).toString('hex')}`;
+    const operatorPublicKeyHash = `0x${bitcoin.crypto.ripemd160(bitcoin.crypto.sha256(operatorKeyPair.publicKey)).toString('hex')}`;
 
     const txb = new bitcoin.TransactionBuilder(DOGECOIN);
     txb.setVersion(1);
     txb.addInput('edbbd164551c8961cf5f7f4b22d7a299dd418758b611b84c23770219e427df67', 0);
-    txb.addOutput(operatorDogeAddress2, 1000000);
-    txb.sign(0, keyPair);
+    txb.addOutput(operator2DogeAddress, 1000000);
+    txb.sign(0, operatorKeyPair);
 
     const tx = txb.build();
     const txData = `0x${tx.toHex()}`;
@@ -38,7 +39,6 @@ contract('testDogeTokenNoOperatorOutput', function(accounts) {
     const dogeToken = await DogeToken.new(trustedRelayerContract, trustedDogeEthPriceOracle, collateralRatio);
 
     const operatorEthAddress = accounts[3];
-
     await dogeToken.addOperatorSimple(operatorPublicKeyHash, operatorEthAddress);
 
     const superblockSubmitterAddress = accounts[4];
