3196d22190fbb4ac78059207d35d5bafd1bd4740
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2019-09-12 15:00:22 -0300

    Update for Truffle v5

--- truffle-config.js
+++ truffle-config.js
@@ -4,42 +4,47 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 47000000000,
+      gas: "47000000000",
       gasPrice: 1
     },
     integrationDogeMain: {
       host: "localhost",
       port: 8545,
       network_id: "32000",
-      gas: 4700000,
+      gas: "4700000",
       gasPrice: 1
     },
     integrationDogeRegtest: {
       host: "localhost",
       port: 8545,
       network_id: "32001",
-      gas: 4700000,
+      gas: "4700000",
       gasPrice: 1
     },
     ropsten: {
       host: "localhost",
       port: 8545,
       network_id: "3", // Ropsten
-      gas: 1000000,
+      gas: "1000000",
       gasPrice: "20000000000"
     },
     rinkeby: {
       host: "localhost",
       port: 8545,
       network_id: "4", // Rinkeby
-      gas: 1000000,
+      gas: "1000000",
       gasPrice: "2000000000"
     }
   },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
+  compilers: {
+    solc: {
+      version: "0.4.25",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
     }
   }
 };
