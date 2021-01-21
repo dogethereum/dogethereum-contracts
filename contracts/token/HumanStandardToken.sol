6ab5eff9f917301f34bac3198868e79b45ba2782
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2021-01-21 19:46:16 -0300

    solidity 0.7.6: "call()" now return (bool, bytes memory)

--- contracts/token/HumanStandardToken.sol
+++ contracts/token/HumanStandardToken.sol
@@ -53,7 +53,8 @@ contract HumanStandardToken is StandardToken {
         //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
         //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
         //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
-        require(_spender.call(abi.encodeWithSignature("receiveApproval(address,uint256,address,bytes)", msg.sender, _value, this, _extraData)));
+        (bool success2,) = _spender.call(abi.encodeWithSignature("receiveApproval(address,uint256,address,bytes)", msg.sender, _value, this, _extraData));
+        require(success2);
         return true;
     }
 }
