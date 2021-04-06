74a80e09161a462825e13a7dcea954d4f39b0814
Sebastián Claudio Nale <sebinale@gmail.com>
2021-04-06 16:52:59 -0300

    Fix several issues in the integration test shell script.
    The send back and forth test is now working.
    
    A few adjustments were made to the logic that greps for leftover dogecoin or
    ganache processes but since this is probably unreliable across
    platforms, a kill command was added at the end to ensure cleanup when
    the script is successful.

--- scripts/send-back-and-forth-test.sh
+++ scripts/send-back-and-forth-test.sh
@@ -4,39 +4,56 @@
 # Tested on Mac OSX only
 
 # Declare variables
-dogecoinQtProcessName=Dogecoin-Qt
-dogecoinQtDatadir=/Users/youruser/Library/Application\ Support/Dogecoin
-dogecoinQtExecutable=/Applications/Dogecoin-Qt.app/Contents/MacOS/Dogecoin-Qt
-dogecoinQtRpcuser=aaa
-dogecoinQtRpcpassword=bbb
-agentCodeDir=/path/agentCodeDir
-agentDataDir=/path/agentDataDir
-dogethereumContractsCodeDir=/path/dogethereumContractsCodeDir
-
 NETWORK="integrationDogeRegtest"
 
+dogecoinQtProcessName=dogecoin-qt
+dogecoinQtDatadir=.dogecoin-data
+dogecoinQtExecutable=dogecoin-qt
+dogecoinQtRpcuser=aaa
+dogecoinQtRpcpassword=bbb
+
+# TODO: we probably want to store all temporary data in a single directory so cleanup is straightforward.
+agentCodeDir=/path/agentCodeDir
+agentDataDir=/path/agentDataDir
+dogethereumDeploymentJson="deployment/$NETWORK/deployment.json"
+
+# We avoid typechecking to speed up execution
+export TS_NODE_TRANSPILE_ONLY=true
+
 # Print instructions on the console
-set -o xtrace -o nounset
+set -o xtrace -o nounset -o errexit
 # Stop dogecoin-qt
-killall $dogecoinQtProcessName
-sleep 3s
+DOGECOIN_PROCESSES="$(pgrep $dogecoinQtProcessName)" || echo "No dogecoin processes found"
+if [[ $DOGECOIN_PROCESSES ]]; then
+	kill "$DOGECOIN_PROCESSES"
+	sleep 1s
+fi
 # Replace dogecoin-qt regtest datadir with the prepared db
 rm -rf "$dogecoinQtDatadir/regtest/"
 unzip "$agentCodeDir/data/doge-qt-regtest-datadir.zip" -d "$dogecoinQtDatadir" > /dev/null 2>&1
 # Start dogecoin-qt
-$dogecoinQtExecutable -regtest -debug -server -listen -rpcuser=$dogecoinQtRpcuser -rpcpassword=$dogecoinQtRpcpassword -rpcport=41200 &
-sleep 10s
+$dogecoinQtExecutable -datadir="$dogecoinQtDatadir" -regtest -debug -server -listen -rpcuser=$dogecoinQtRpcuser -rpcpassword=$dogecoinQtRpcpassword -rpcport=41200 &
+dogecoinNode=$!
+sleep 4s
 # Mine a doge block so dogecoin-qt is fully up and running
 curl --user $dogecoinQtRpcuser:$dogecoinQtRpcpassword  --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "generate", "params": [1] }' -H 'content-type: text/plain;' http://127.0.0.1:41200/
 # Clear agent data dir
 # TODO: is this necessary?
 rm -rf ${agentDataDir:?}/*
-# Restart ganache
-kill "$(pgrep ganache)"
-sleep 1s
+# Stop ganache
+GANACHE_PROCESSES="$(pgrep --full '^node.*ganache-cli')" || echo "No ganache processes found"
+if [[ $GANACHE_PROCESSES ]]; then
+	# kill fails if passed an empty string
+	kill "$GANACHE_PROCESSES"
+	sleep 1s
+fi
+# Start ganache
 npm run ganache > ganachelog.txt &
+ganacheNode=$!
 # Compile and deploy contracts
 npx hardhat compile --quiet
+# Deploy dogethereum to Ethereum network
+rm -rf "deployment/$NETWORK"
 npx hardhat run --network $NETWORK scripts/deployDogethereum.ts
 # Init contracts: initial doge header and operator
 npx hardhat run --network $NETWORK scripts/init_contracts_local.ts
@@ -49,12 +66,12 @@ npx hardhat run --network $NETWORK scripts/wait_token_balance.ts
 npx hardhat run --network $NETWORK scripts/prepare_sender.ts
 for i in {1..2}; do
 	# Send eth unlock tx
-	node ../dogethereum-tools/user/unlock.js --ethnetwork ganacheDogeRegtest --json $dogethereumContractsCodeDir/build/contracts/DogeToken.json --sender 0xd2394f3fad76167e7583a876c292c86ed10305da --receiver ncbC7ZY1K9EcMVjvwbgSBWKQ4bwDWS4d5P --value 300000000
+	node ../dogethereum-tools/user/unlock.js --deployment $dogethereumDeploymentJson --privateKey 0xf968fec769bdd389e33755d6b8a704c04e3ab958f99cc6a8b2bcf467807f9634 --receiver ncbC7ZY1K9EcMVjvwbgSBWKQ4bwDWS4d5P --value 300000000
 	# Print debug.js status
 	npx hardhat run --network $NETWORK scripts/debug.ts
 	# Mine 5 eth blocks so unlock eth tx has enough confirmations
 	for j in {1..5}; do
-		curl -X POST --data '{"jsonrpc":"2.0","method":"evm_mine","params":[],"id":74}' http://localhost:8545;
+		curl --request POST --data '{"jsonrpc":"2.0","method":"evm_mine","params":[],"id":74}' http://localhost:8545;
 	done
 	# Wait for Eth to Doge agent to sign and broadcast doge unlock tx
 	sleep 30s
@@ -63,3 +80,5 @@ for i in {1..2}; do
 	# Wait for agent to relay doge unlock tx to eth and utxo length updated
 	npx hardhat run --network $NETWORK scripts/wait_two_utxos.ts
 done
+
+kill $dogecoinNode $ganacheNode
