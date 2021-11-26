5c7b17aed159a4129cbf2548855830fce0b12b80
Sebastián Claudio Nale <sebinale@gmail.com>
2021-11-26 14:22:42 -0300

    Auctions: bot CLI.
    Includes a sample config.

--- auction-bot/cli.ts
+++ auction-bot/cli.ts
@@ -1,11 +1,57 @@
-import {auctionBot} from "./main";
+import {ethers, getDefaultProvider} from "ethers";
+import yargs from "yargs";
+import fs from "fs/promises";
 
-auctionBot()
+import { createConfig, isUserConfig } from "./config";
+import { AuctionBot } from "./main";
+
+const pollingPeriod = 3000;
+
+async function main() {
+  const args = await yargs.alias("c", "config").string("config").demandOption("config").parse();
+  const json = await fs.readFile(args.config, {
+    encoding: "utf8",
+  });
+  const userConfig = JSON.parse(json);
+  if (!isUserConfig(userConfig)) {
+    // TODO: specify which are missing
+    throw new Error("Missing or wrong fields in configuration.");
+  }
+  const provider = ethers.getDefaultProvider(userConfig.ethereumNodeURL);
+  const signer = new ethers.Wallet(userConfig.bidderPrivateKey, provider);
+  const config = createConfig(userConfig);
+  if (!ethers.utils.isAddress(config.auctionAddress)) {
+    throw new Error(
+      "Provided auction address is not a valid Ethereum address."
+    );
+  }
+  const bot = await AuctionBot.create(signer, config, ethers);
+
+  const state = { stop: false };
+  const stopper = () => {
+    state.stop = true;
+  };
+  process.on("SIGINT", stopper);
+  process.on("SIGTERM", stopper);
+  while (!state.stop) {
+    await bot.processNextBlocks();
+
+    await delay(pollingPeriod);
+  }
+}
+
+function delay(milliseconds: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+main()
   .then(() => {
     console.log("Finished");
     process.exit(0);
   })
   .catch((error) => {
-    console.error(error);
+    console.error(error.stack || error);
     process.exit(1);
   });
--- auction-bot/config.ts
+++ auction-bot/config.ts
@@ -1,6 +1,46 @@
 import type { Options } from "sequelize";
 import type { providers } from "ethers";
 
+/**
+ * User-facing bot configuration.
+ */
+export interface UserConfig {
+  /**
+   * If the database is empty, the bot will start monitoring events at this block.
+   * The starting block should be fairly recent.
+   */
+  startingBlock?: providers.BlockTag;
+  /**
+   * The bot will only read blocks that are confirmed at least this number of times.
+   */
+  numberOfConfirmations?: number;
+  /**
+   * Database file path.
+   */
+  dbPath: string;
+  /**
+   * Amount to bid in an active auction.
+   */
+  bidAmount: string;
+  /**
+   * Auction contract address.
+   * Should be in hex string format.
+   */
+  auctionAddress: string;
+  /**
+   * Private key of the bidder.
+   * Should be in hex string format.
+   */
+  bidderPrivateKey: string;
+  /**
+   * Ethereum node URL.
+   */
+  ethereumNodeURL: string;
+}
+
+/**
+ * Internal bot configuration.
+ */
 export interface Config {
   /**
    * If the database is empty, the bot will start monitoring events at this block.
@@ -21,11 +61,47 @@ export interface Config {
   bidAmount: string;
   /**
    * Auction contract address.
+   * Should be in hex string format.
    */
   auctionAddress: string;
   /**
    * Identifier for the auction indexer.
    * Should always be zero.
+   * E.g. "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
    */
   indexerId: 0;
+}
+
+export function createConfig(partialConfig: UserConfig): Config {
+  return {
+    indexerId: 0,
+    auctionAddress: partialConfig.auctionAddress,
+    bidAmount: partialConfig.bidAmount,
+    numberOfConfirmations: partialConfig.numberOfConfirmations || 10,
+    startingBlock: partialConfig.startingBlock || "latest",
+    sequelizeOptions: {
+      dialect: "sqlite",
+      storage: partialConfig.dbPath,
+      logging: false,
+    },
+  };
+}
+
+export function isUserConfig(config: any): config is UserConfig {
+  return (
+    typeof config.auctionAddress === "string" &&
+    typeof config.bidAmount === "string" &&
+    (typeof config.numberOfConfirmations === "undefined" ||
+      isSafeInteger(config.numberOfConfirmations)) &&
+    (typeof config.startingBlock === "undefined" ||
+      typeof config.startingBlock === "string" ||
+      isSafeInteger(config.startingBlock)) &&
+    typeof config.dbPath === "string" &&
+    typeof config.bidderPrivateKey === "string" &&
+    typeof config.ethereumNodeURL === "string"
+  );
+}
+
+function isSafeInteger(someNumber: any): boolean {
+  return typeof someNumber === "number" && Number.isSafeInteger(someNumber);
 }
